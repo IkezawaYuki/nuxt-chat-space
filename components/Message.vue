bd07b50ea95bafb23d54c92ed00b82b1cd44688b
IkezawaYuki <y.ikezawa93@gmail.com>
2019-10-03 23:40:49 +0900

    modified file

---
+++ components/Message.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="contents__post">
+
+        <div class="contents__post__head">
+            <div class="user_name">
+
+            </div>
+            <div class="date">
+
+            </div>
+        </div>
+        <p v-if="true" class="contents__post__text">
+            メッセージコンテンツ
+        </p>
+
+        <!-- todo -->
+
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
